397158e72b5f10f1a134b0e4049eeee94f159c18
davidho27941 <davidho@gapp.nthu.edu.tw>
2025-06-05 15:43:19 +0800

    chore: init project and add source code.

---
+++ private_captcha_bot/src/main/java/com/david/Main.java
@@ -0,0 +1,20 @@
+package com.david;
+
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String botToken = System.getenv("API_KEY");
+        
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new PrivateCatchaBot(botToken));
+            System.out.println("PrivateCatchaBot successfully started!");
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
