a105972b3641d9244323283d62a5163bc1763909
davidho27941 <davidho@gapp.nthu.edu.tw>
2025-06-12 22:54:11 +0800

    chore: (1) add package def. (2) make class method to public.

--- private_captcha_bot/src/main/java/com/telegram_bot/interfaces/GenericHandler.java
+++ private_captcha_bot/src/main/java/com/telegram_bot/interfaces/GenericHandler.java
@@ -1,13 +1,15 @@
+package com.telegram_bot.interfaces;
+
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+public interface GenericHandler {
 
-public interface CommandHandler {
     /**
      * 處理從 Telegram 收到的 Update（已知一定是有文字的訊息）
      * @param update 完整的 Update 物件
      * @param telegramClient 用來呼叫 execute(...) 發送回覆
      */
-    void handle(Update update, TelegramClient telegramClient) throws TelegramApiException;
+    public void handle(Update update, TelegramClient telegramClient) throws TelegramApiException;
 }
