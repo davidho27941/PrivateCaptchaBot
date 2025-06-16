31ea88c641afcd91803188ba1925cc73149317a1
davidho27941 <davidho@gapp.nthu.edu.tw>
2025-06-16 13:53:03 +0800

    feat: add class to handler restrict user event.

---
+++ private_captcha_bot/src/main/java/com/telegram_bot/handlers/commands/RestrictUserCommandHandler.java
@@ -0,0 +1,147 @@
+package com.telegram_bot.handlers.commands;
+
+import org.telegram.telegrambots.meta.api.objects.ChatPermissions;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.RestrictChatMember;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import com.telegram_bot.interfaces.GenericHandler;
+
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParameterException;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+import picocli.CommandLine.Help.Ansi;
+
+class RestrictUserCommandOptions {
+    @Parameters
+    List<String> params;
+
+    @Option(names = {"--message"}, defaultValue = "false", description = "Can send messages.")
+    boolean can_send_messages;
+
+    @Option(names = {"--audio"}, defaultValue = "false", description = "Can send audios.")
+    boolean can_send_audios;
+
+    @Option(names = {"--documents"}, defaultValue = "false", description = "Can send documents.")
+    boolean can_send_documents;
+
+    @Option(names = {"--photo"}, defaultValue = "false", description = "Can send photos.")
+    boolean can_send_photos;
+
+    @Option(names = {"--video"}, defaultValue = "false", description = "Can send video.")
+    boolean can_send_videos;
+
+    @Option(names = {"--video-note"}, defaultValue = "false", description = "Can send video note.")
+    boolean can_send_video_notes;
+
+    @Option(names = {"--voice-note"}, defaultValue = "false", description = "Can send voice note.")
+    boolean can_send_voice_notes;
+
+    @Option(names = {"--poll"}, defaultValue = "false", description = "Can send polls.")
+    boolean can_send_polls;
+
+    @Option(names = {"--other-messages"}, defaultValue = "false", description = "Can send other messages.")
+    boolean can_send_other_messages;
+
+    @Option(names = {"--web-preview"}, defaultValue = "false", description = "Can send web previews.")
+    boolean can_add_web_page_previews;
+
+    @Option(names = {"--change-info"}, defaultValue = "false", description = "Can change info.")
+    boolean can_change_info;
+
+    @Option(names = {"--invite"}, defaultValue = "false", description = "Can sinvite users.")
+    boolean can_invite_users;
+
+    @Option(names = {"--pin-message"}, defaultValue = "false", description = "Can pin message.")
+    boolean can_pin_messages;
+
+    @Option(names = {"--manage-topic"}, defaultValue = "false", description = "Can manage topic.")
+    boolean can_manage_topics;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Provide help message.")
+    boolean helpRequested;
+
+}
+
+
+public class RestrictUserCommandHandler extends CommandHandler {
+
+    @Override
+    public void handle(Update update, TelegramClient telegramClient) throws TelegramApiException {
+
+        long chat_id = update.getMessage().getChatId();
+        int message_id = update.getMessage().getMessageId();
+
+        String[] argv = update.getMessage().getText().split("\\s+");
+
+        RestrictUserCommandOptions opts = new RestrictUserCommandOptions();
+
+        CommandLine cmd = new CommandLine(opts);
+
+        try {
+
+            CommandLine.ParseResult result = cmd.parseArgs(argv);
+
+        } catch (ParameterException e) {
+            e.getMessage();
+        }
+
+        if (opts.helpRequested) {
+            showHelpMessage(update, telegramClient, cmd, chat_id, message_id);
+
+        } else {
+            
+            System.out.println(opts.can_send_messages);
+
+            long user_id = Long.parseLong(opts.params.get(1));
+
+            ChatPermissions permission = ChatPermissions
+                .builder()
+                .canSendMessages(opts.can_send_messages)
+                .canSendAudios(opts.can_send_audios)
+                .canSendDocuments(opts.can_send_documents)
+                .canSendPhotos(opts.can_send_photos)
+                .canSendVideos(opts.can_send_videos)
+                .canSendVideoNotes(opts.can_send_video_notes)
+                .canSendVoiceNotes(opts.can_send_voice_notes)
+                .canSendPolls(opts.can_send_polls)
+                .canSendOtherMessages(opts.can_send_other_messages)
+                .canAddWebPagePreviews(opts.can_add_web_page_previews)
+                .canChangeInfo(opts.can_change_info)
+                .canInviteUsers(opts.can_invite_users)
+                .canPinMessages(opts.can_pin_messages)
+                .canManageTopics(opts.can_manage_topics)
+                .build();
+
+            RestrictChatMember restriction = RestrictChatMember
+                .builder()
+                .chatId(chat_id)
+                .userId(user_id)
+                .permissions(permission)
+                .build();
+            
+            try {
+                telegramClient.execute(restriction);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+                return;
+            }
+            
+            // String message = "User " + user_id + " has beed unbanned.";
+
+            sendMessage(update, telegramClient, chat_id, message, message_id);
+
+        }
+
+    }
+
+}
