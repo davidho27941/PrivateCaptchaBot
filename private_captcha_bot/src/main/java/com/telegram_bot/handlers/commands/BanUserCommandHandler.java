2947b0c56315e01f6376c41f857c413c3a31eb1a
davidho27941 <davidho@gapp.nthu.edu.tw>
2025-06-17 09:22:29 +0800

    chore & style: add formatter and apply to script.

--- private_captcha_bot/src/main/java/com/telegram_bot/handlers/commands/BanUserCommandHandler.java
+++ private_captcha_bot/src/main/java/com/telegram_bot/handlers/commands/BanUserCommandHandler.java
@@ -1,115 +1,108 @@
 package com.telegram_bot.handlers.commands;
 
 // import org.telegram.telegrambots.meta.api.objects.ChatPermissions;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.generics.TelegramClient;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import org.telegram.telegrambots.meta.api.methods.groupadministration.BanChatMember;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-
-import com.telegram_bot.interfaces.GenericHandler;
-
-import picocli.CommandLine.Model.CommandSpec;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 import picocli.CommandLine.ParameterException;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.List;
-import picocli.CommandLine.Help.Ansi;
-
-import java.time.Duration;
-import java.time.Instant;;
-import java.time.temporal.ChronoUnit;
+import picocli.CommandLine.Parameters;
 
 class BanUserCommandOptions {
-    @Parameters
-    List<String> params;
+  @Parameters List<String> params;
 
-    @Option(names = {"-d", "--duration"}, description = "Ban duration.")
-    int duration;
+  @Option(
+      names = {"-d", "--duration"},
+      description = "Ban duration.")
+  int duration;
 
-    @Option(names = {"--del"}, defaultValue="false", description="Remove message or not.")
-    boolean revoke_messages;
+  @Option(
+      names = {"--del"},
+      defaultValue = "false",
+      description = "Remove message or not.")
+  boolean revoke_messages;
 
+  @Option(
+      names = {"--uid"},
+      description = "User ID.")
+  long uid;
 
-    @Option(names = {"--uid"}, description = "User ID.")
-    long uid;
+  @Option(
+      names = {"-h", "--help"},
+      usageHelp = true,
+      description = "Provide help message.")
+  boolean helpRequested;
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Provide help message.")
-    boolean helpRequested;
-
-    @Option(names = {"-u", "--unit"}, 
-        description = "單位：${COMPLETION-CANDIDATES}（預設 ${DEFAULT-VALUE}）",
-        defaultValue = "MINUTES")
-    ChronoUnit unit;
+  @Option(
+      names = {"-u", "--unit"},
+      description = "單位：${COMPLETION-CANDIDATES}（預設 ${DEFAULT-VALUE}）",
+      defaultValue = "MINUTES")
+  ChronoUnit unit;
 }
-
 
 public class BanUserCommandHandler extends CommandHandler {
 
-    @Override
-    public void handle(Update update, TelegramClient telegramClient) throws TelegramApiException {
+  @Override
+  public void handle(Update update, TelegramClient telegramClient) throws TelegramApiException {
 
-        long chat_id = update.getMessage().getChatId();
-        int message_id = update.getMessage().getMessageId();
+    long chat_id = update.getMessage().getChatId();
+    int message_id = update.getMessage().getMessageId();
 
-        String[] argv = update.getMessage().getText().split("\\s+");
+    String[] argv = update.getMessage().getText().split("\\s+");
 
-        BanUserCommandOptions opts = new BanUserCommandOptions();
+    BanUserCommandOptions opts = new BanUserCommandOptions();
 
-        CommandLine cmd = new CommandLine(opts);
+    CommandLine cmd = new CommandLine(opts);
 
-        try {
+    try {
 
-            CommandLine.ParseResult result = cmd.parseArgs(argv);
+      CommandLine.ParseResult result = cmd.parseArgs(argv);
 
-        } catch (ParameterException e) {
-            e.getMessage();
-        }
-
-        if (opts.helpRequested) {
-            showHelpMessage(update, telegramClient, cmd, chat_id, message_id);
-
-        } else {
-
-            Instant now = Instant.now();
-
-            Instant future = now.plus(opts.duration, opts.unit);
-            
-            long futureEpochSeconds = future.getEpochSecond();
-            int futureSecondsInt = Math.toIntExact(futureEpochSeconds);
-
-            Integer futureSecondsInteger = futureSecondsInt;
-
-            System.out.println(opts.revoke_messages);
-
-            long user_id = Long.parseLong(opts.params.get(1));
-
-            BanChatMember banAction = BanChatMember
-                .builder()
-                .chatId(chat_id)
-                .userId(user_id)
-                .untilDate(futureSecondsInt)
-                .revokeMessages(opts.revoke_messages)
-                .build();
-
-            try {
-                telegramClient.execute(banAction);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-                return;
-            }
-            
-            String message = "User " + user_id + " has beed banned.";
-
-            sendMessage(update, telegramClient, chat_id, message, message_id);
-
-        }
-
+    } catch (ParameterException e) {
+      e.getMessage();
     }
 
+    if (opts.helpRequested) {
+      showHelpMessage(update, telegramClient, cmd, chat_id, message_id);
+
+    } else {
+
+      Instant now = Instant.now();
+
+      Instant future = now.plus(opts.duration, opts.unit);
+
+      long futureEpochSeconds = future.getEpochSecond();
+      int futureSecondsInt = Math.toIntExact(futureEpochSeconds);
+
+      Integer futureSecondsInteger = futureSecondsInt;
+
+      System.out.println(opts.revoke_messages);
+
+      long user_id = Long.parseLong(opts.params.get(1));
+
+      BanChatMember banAction =
+          BanChatMember.builder()
+              .chatId(chat_id)
+              .userId(user_id)
+              .untilDate(futureSecondsInt)
+              .revokeMessages(opts.revoke_messages)
+              .build();
+
+      try {
+        telegramClient.execute(banAction);
+      } catch (TelegramApiException e) {
+        e.printStackTrace();
+        return;
+      }
+
+      String message = "User " + user_id + " has beed banned.";
+
+      sendMessage(update, telegramClient, chat_id, message, message_id);
+    }
+  }
 }
